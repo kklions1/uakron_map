6aa0c3ea833ba07c6afeaacd73a04c04e201dfdf
Kevin Klions <kklions1@gmail.com>
2018-11-24 23:33:35 -0500

    add LocationNames interface

---
+++ app/src/main/java/com/example/kevin/mapdatabasesproject/database/contract/LocationNames.java
@@ -0,0 +1,15 @@
+package com.example.kevin.mapdatabasesproject.database.contract;
+
+/**
+ * Has all the names used in the locations table so they are consistent across the app
+ */
+public interface LocationNames {
+    String STUDENT_UNION = "Student Union";
+    String POLSKY = "Polsky Hall";
+    String CROUSE = "Crouse Hall";
+    String REC_CENTER = "Student Rec Center";
+    String ARTS_AND_SCIENCES = "College of Arts and Sciences";
+    String BUSINESS_ADMIN = "College of Business Administration";
+    String KOLBE = "Kolbe Hall";
+    String LEIGH = "Leigh Hall";
+}
