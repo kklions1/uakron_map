94202ff0cdc287b12051a6dfe78bbd156c6ffcdf
kklions1 <kklions1@gmail.com>
2019-02-26 15:13:28 -0500

    added okhttp dependency, added RequestManager class

---
+++ app/src/main/java/com/example/kevin/mapdatabasesproject/manager/HTTPRequestManager.java
@@ -0,0 +1,32 @@
+package com.example.kevin.mapdatabasesproject.manager;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+public class HTTPRequestManager {
+    private String appURL = "https://secure-outpost-229516.appspot.com/";
+
+
+    public class TestNetworkResponse extends AsyncTask<String, Void, String> {
+        @Override
+        public String doInBackground(String... strings) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(strings[0])
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                return response.body().string();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        public void onPostExecute(String result) {
+            TextView resultView = findViewById(R.id.result_view);
+            resultView.setText(result);
+        }
+    }
+}
