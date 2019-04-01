c2cc8265d643186b25695cbb04eb9a076bcc2628
kklions1 <kklions1@gmail.com>
2019-04-01 15:13:19 -0400

    added kotlin support, and some prototype kotlin code

---
+++ app/src/main/java/com/example/kevin/mapdatabasesproject/manager/DataManager.kt
@@ -0,0 +1,16 @@
+//package com.example.kevin.mapdatabasesproject.manager
+//
+//import com.example.kevin.mapdatabasesproject.model.Course
+//import com.google.gson.Gson
+//import com.google.gson.JsonArray
+//
+//fun parseJsonResponse(json: String): List<Course> {
+//    var gson = Gson()
+//    var result: List<Course>
+//
+//    var responseArray = gson.fromJson(json, JsonArray::class)
+//    for (i in 0..responseArray.size()) {
+//
+//        )
+//
+//}
